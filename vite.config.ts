a5bf87e9ff72fb0fb506f3127653b6292a2555cd
Eray Genç <eraygench@gmail.com>
2026-02-21 01:02:28 +0300

    add `nitro` plugin to Vite config and update dependencies in pnpm lockfile

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   server: {
@@ -11,6 +12,7 @@ export default defineConfig({
   },
   plugins: [
     devtools(),
+    nitro(),
     tailwindcss(),
     tanstackStart({
       srcDirectory: 'src', // This is the default
